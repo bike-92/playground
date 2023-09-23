42737019f38460b6b221028f13f320c283c4cb7f
bike-92 <awh.daemen@gmail.com>
2023-09-23 08:36:27 +0200

    create components menubar and menubar buttons

---
+++ my-app/src/app/menubar/menubuttons-main/menubuttons-main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menubuttons-main',
+  templateUrl: './menubuttons-main.component.html',
+  styleUrls: ['./menubuttons-main.component.css']
+})
+export class MenubuttonsMainComponent {
+
+}
