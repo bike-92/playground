c196f6a5c7d3e63543958398c907f21c99181d9e
bike-92 <awh.daemen@gmail.com>
2023-09-23 09:03:12 +0200

    add colour background color and buttons

--- my-app/src/app/menubar/menubar.component.ts
+++ my-app/src/app/menubar/menubar.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./menubar.component.css']
 })
 export class MenubarComponent {
+  public highlight = false;
 
-}
+  ngOnInit(){
+    console.log('highlight: ', this.highlight);
+  }
+
+  public alertMe(): void {
+    console.log(' I have been clicked');
+      alert(" I have been clicked!");
+  }
+
+  public hoverOver(): void {
+    console.log('hovering over button ', this.highlight);
+    this.highlight = !this.highlight;
+    console.log('highlight: ', this.highlight);
+  }
+  }
--- my-app/src/app/menubar/menubuttons-main/menubuttons-main.component.ts
+++ my-app/src/app/menubar/menubuttons-main/menubuttons-main.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./menubuttons-main.component.css']
 })
 export class MenubuttonsMainComponent {
-
 }
